09362a01ad3fb2a57ca9d2070af760db934f971e
Vivian Lin, Jack Fernbacher, William Tan <vivianlin.is@gmail.com>
2016-08-24 16:08:33 -0700

    second commit

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -1,49 +1,69 @@
 class CarController < ApplicationController
   def create
-    @make = params[:make]
-    @model_year = params[:model_year]
-    @color = params[:color]
-    @car = Car.new(@make,@model_year, @color)
-    @parking_brake_on = @car.get_parking_brake
-    @parking_brake_off = !@car.get_parking_brake
-    session[:car] = @car.to_yaml
+    if (!params[:make].nil? && params[:make].strip.empty?) || (!params[:model_year].nil? && params[:model_year].strip.empty?)
+      redirect_to '/'
+    else
+      @make = params[:make]
+      @model_year = params[:model_year]
+      @color = params[:color]
+      @car = Car.new(@make,@model_year, @color)
+      @parking_brake_on = @car.get_parking_brake
+      @parking_brake_off = !@car.get_parking_brake
+      session[:car] = @car.to_yaml
+    end
   end
 
   def accelerate
-    @car = YAML.load(session[:car])
-    @car.accelerate
-    @parking_brake_on = @car.get_parking_brake
-    @parking_brake_off = !@car.get_parking_brake
-    session[:car] = @car.to_yaml
-    render 'create.html.erb'
+    if !session.has_key?("car")
+      redirect_to '/'
+    else
+      @car = YAML.load(session[:car])
+      @car.accelerate
+      @parking_brake_on = @car.get_parking_brake
+      @parking_brake_off = !@car.get_parking_brake
+      session[:car] = @car.to_yaml
+      render 'create.html.erb'
+    end
   end
 
   def brake
-    @car = YAML.load(session[:car])
-    @car.brake
-    @parking_brake_on = @car.get_parking_brake
-    @parking_brake_off = !@car.get_parking_brake
-    session[:car] = @car.to_yaml
-    render 'create.html.erb'
+    if !session.has_key?("car")
+      redirect_to '/'
+    else
+      @car = YAML.load(session[:car])
+      @car.brake
+      @parking_brake_on = @car.get_parking_brake
+      @parking_brake_off = !@car.get_parking_brake
+      session[:car] = @car.to_yaml
+      render 'create.html.erb'
+    end
   end
 
   def toggle_lights
-    @car = YAML.load(session[:car])
-    @speed = @car.get_speed
-    @car.toggle_lights
-    @parking_brake_on = @car.get_parking_brake
-    @parking_brake_off = !@car.get_parking_brake
-    session[:car] = @car.to_yaml
-    render 'create.html.erb'
+    if !session.has_key?("car")
+      redirect_to '/'
+    else
+      @car = YAML.load(session[:car])
+      @speed = @car.get_speed
+      @car.toggle_lights
+      @parking_brake_on = @car.get_parking_brake
+      @parking_brake_off = !@car.get_parking_brake
+      session[:car] = @car.to_yaml
+      render 'create.html.erb'
+    end
   end
 
   def toggle_parking_brake
-    @car = YAML.load(session[:car])
-    @car.toggle_parking_brake(params[:parking_brake])
-    @parking_brake_on = @car.get_parking_brake
-    @parking_brake_off = !@car.get_parking_brake
-    session[:car] = @car.to_yaml
-    render 'create.html.erb'
+    if !session.has_key?("car")
+      redirect_to '/'
+    else
+      @car = YAML.load(session[:car])
+      @car.toggle_parking_brake(params[:parking_brake])
+      @parking_brake_on = @car.get_parking_brake
+      @parking_brake_off = !@car.get_parking_brake
+      session[:car] = @car.to_yaml
+      render 'create.html.erb'
+    end
   end
 
   def index
